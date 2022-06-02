fb71579d8503ea711cb290d6738b8125a4b302b0
edosatk <gendabusen@gmail.com>
2022-06-02 16:10:28 -0700

    C-Hello world project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/*
+ * main -prints out a string to stdout.
+ * return: Always 0 success.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
