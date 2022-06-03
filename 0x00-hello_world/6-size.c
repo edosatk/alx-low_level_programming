7d07e9de764ab348b0b9306bd9f62d25c420337a
edosatk <gendabusen@gmail.com>
2022-06-02 17:00:53 -0700

    C-Hello world project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: Always 0 success.
+ */
+int main(void)
+{
+char charType;	
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of char: %zu byte\n", sizeof(intType));
+printf("Size of char: %zu byte\n", sizeof(longintType));
+printf("Size of char: %zu byte\n", sizeof(longlongintType));
+printf("Size of char: %zu byte\n", sizeof(floatType));
+return (0)
+}
