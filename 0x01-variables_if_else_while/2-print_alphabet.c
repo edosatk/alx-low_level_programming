083389e47b79100e196f6568fba56b3758965e39
edosatk <gendabusen@gmail.com>
2022-06-03 18:13:29 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,11 +10,14 @@
 
 int main(void)
 {
-	char low;
+	char a = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	while (a <= 'z')
+{
+	putchar(a);
+	       a++;
+}
+        putchar('\n');
 
 	return (0);
 }
