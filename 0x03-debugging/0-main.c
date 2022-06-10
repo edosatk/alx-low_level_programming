76a4855e3fabb01f91e9cc8934ff7440ed295137
edosatk <gendabusen@gmail.com>
2022-06-09 18:30:02 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - check the code
+ * Return: Always 0.
  */
+#include "stdlib.h"
+#include "main.h"
+
 int main(void)
 {
-        int n;
+	int i;
+	i = 0;
+	positive_or_negative(i);
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-
-        if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n == 0)
-                printf("%d is zero\n", n);
-        else
-                printf("%d is negative\n", n);
-
-        return (0);
+	return (0);
 }
