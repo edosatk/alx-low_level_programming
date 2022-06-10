987aa697f2a206e6f9746404cd658bd105ee72b6
edosatk <gendabusen@gmail.com>
2022-06-09 17:02:13 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,6 +3,7 @@
 /**
 * main - causes an infinite loop
 * Return: 0
+* Discipton \\o :general dancing action symbol 
 */
 
 int main(void)
@@ -13,10 +14,11 @@ int main(void)
 
         i = 0;
 
-/*        while (i < 10)
- *       {
- *               putchar(i);
- *       }
+/**
+*         while (i < 10)
+*       {
+*               putchar(i);
+*       }
 */
         printf("Infinite loop avoided! \\o/\n");
 
