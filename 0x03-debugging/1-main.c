e16b7534d5220e6d2ab361c35c349b8f4fb0e602
edosatk <gendabusen@gmail.com>
2022-06-09 17:30:06 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,8 @@
-/* infinite loop */
 #include <stdio.h>
 
 /**
 * main - causes an infinite loop
-* return:0
-* Description \\o/\n:general dancing action symbol
-* infinite loop
+* Return: 0
 */
 
 int main(void)
@@ -15,14 +12,14 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
 /**
-*         while (i < 10)
-*       {
-*               putchar(i);
-*       }
+*        while (i < 10)
+*        {
+*                putchar(i);
+*        }
 */
-printf("Infinite loop avoided! \\o/\n");
+        printf("Infinite loop avoided! \\o/\n");
 
-return (0);
+        return (0);
 }
+
