8a29a607edc587dc469ba5ba053a2f496a34af68
edosatk <gendabusen@gmail.com>
2022-06-22 18:38:54 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strlen_recursion - gives length of a string
+* @s: string to be measured
+*Return: length of the string
+*/
+int _strlen_recursion(char *s){
+    int i=1;
+   
+    if (*s == '\0')
+    {
+        return;
+    }
+    i+=_strlen_recursion(s+1);
+    return(i);
+    
+     
+}
