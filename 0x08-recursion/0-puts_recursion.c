29d9acd61ad77a0c674c6c0ff56c4a6b568ae9d5
edosatk <gendabusen@gmail.com>
2022-06-22 17:37:41 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string followed by a new line
+*@s:character to be printed
+*Return:returns void
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0'){
+	retur;
+	}
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	
+}
